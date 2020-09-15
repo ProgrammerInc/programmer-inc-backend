2e9d0d589587cc2c31c158656518e6769cc0f685
Programmer Incorporated <programmer.incorporated@gmail.com>
2020-09-14 23:03:03 -0500

    Initial commit.

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05630396-2e06-4d8a-a787-e8f09185c1cd'
+};
---
+++ seed/seed.js
@@ -0,0 +1,23 @@
+module.exports = {
+  categories: [{ name: "News" }, { name: "Trends" }],
+  articles: [
+    {
+      mimeType: "image/jpeg",
+      imageFileName: "blog-header-network.jpg",
+      published_at: new Date("2020-09-12"),
+      category: 1,
+      title: "Welcome to Programmer Incorporated",
+      content: `
+      # Happy Day of the Programmer!
+
+      We decided to begin this new venture with [100 Days of Code](https://www.100daysofcode.com/).
+
+      Starting on the [Day of the Programmer](https://en.wikipedia.org/wiki/Day_of_the_Programmer), September, 12th, 2020.
+
+      Follow our journey as we build new tools and features everyday.
+
+      Enjoy!
+      `,
+    },
+  ],
+};
